559c0a6a78ccaa721c501a653f36631ae62758b0
pirokinhhaplay277 <nicolasmfreitas20@gmail.com>
2025-08-13 19:34:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
- 
-    botaoDeAcessibilidade.addEventListener('click', function (){
-     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
- 
-     const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
-     botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
-   
-    })
- 
-     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-     
-     const alternaContraste = document.getElementById('alterna-contraste')
- 
-     let tamanhoAtualFonte = 1;
- 
-     aumentaFonteBotao.addEventListener('click', function(){
-         tamanhoAtualFonte += 0.1;
-         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
- 
-     })
- 
-     diminuiFonteBotao.addEventListener('click', function(){
-         tamanhoAtualFonte -= 0.1;
-         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
- 
-     })
- 
-     alternaContraste.addEventListener('click', function(){
-         document.body.classList.toggle('alto-contraste')
-     })
- 
- 
- })
- 
- ScrollReveal().reveal('#inicio', { delay: 500 });
- ScrollReveal().reveal('#tropicalia', { delay: 500 });
- ScrollReveal().reveal('#galeria', { delay: 500 });
- ScrollReveal().reveal('#contato', { delay: 500 });
+// Acessibilidade: Aumentar ou diminuir a fonte
+let tamanhoFonte = 100;
+
+document.getElementById('aumentar-fonte').addEventListener('click', () => {
+    if (tamanhoFonte < 150) {
+        tamanhoFonte += 10;
+        document.body.style.fontSize = `${tamanhoFonte}%`;
+    }
+});
+
+document.getElementById('diminuir-fonte').addEventListener('click', () => {
+    if (tamanhoFonte > 80) {
+        tamanhoFonte -= 10;
+        document.body.style.fontSize = `${tamanhoFonte}%`;
+    }
+});
